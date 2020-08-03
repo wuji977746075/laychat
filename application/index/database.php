73f8b2829e480c72f64dfeb0a24983c720adedd0
rainbow <hzboye010@163.com>
2020-08-03 14:14:44 +0800

    init  ..

--- application/index/database.php
+++ application/index/database.php
@@ -5,11 +5,11 @@ return [
     // 服务器地址
     'hostname'       => '127.0.0.1',
     // 数据库名
-    'database'       => 'laychat',
+    'database'       => 'snake',
     // 用户名
     'username'       => 'root',
     // 密码
-    'password'       => 'root',
+    'password'       => '1',
     // 端口
     'hostport'       => '',
     // 连接dsn
